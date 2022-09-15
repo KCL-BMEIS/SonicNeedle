025b082a09bfbe786db598ae2a5339c2b14b428f
crnbaker <christian.baker@kcl.ac.uk>
2022-09-15 19:04:50 +0100

    Mock pulser class implemented

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from abc import ABC, abstractmethod
 from copy import copy
-from math import floor
+from math import floor, pi, sin
 from queue import Empty, Queue
 from threading import Thread
 from typing import Any
@@ -15,12 +16,8 @@ SOUND_SPEED_MPS = 343.0
 MAX_DISTANCE_CM = 20
 
 
-class Pulser(serial.Serial):
-    def __init__(self, output_queue: Queue, rate_hz: float, com: str = '/dev/cu.usbmodem101', baud: int = 9600):
-        super().__init__(com, baud, timeout=.1)
-        print('Waiting for device...')
-        sleep(3)
-        print('Connected to ', self.name)
+class Pulser(ABC):
+    def __init__(self, output_queue: Queue, rate_hz: float):
         self._is_running = True
         self._output_queue = output_queue
         self._pulse_thread = Thread(target=self._pulse_thread_function, args=(self._output_queue,))
@@ -45,14 +42,38 @@ class Pulser(serial.Serial):
         self._is_running = False
         self._pulse_thread.join()
 
-    def _read_time_of_flight(self) -> float:
-        return 1e-6 * float(str(self.readline().decode().strip('\r\n')))
-
     def _pulse_thread_function(self, output_queue: Queue) -> None:
         while self._is_running:
-            self.write(b'0')
             output_queue.put(SOUND_SPEED_MPS * self._read_time_of_flight() * 0.5)
             sleep(1 / self._rate)
+
+    @abstractmethod
+    def _read_time_of_flight(self) -> float:
+        raise NotImplementedError()
+
+
+class ArduinoPulser(Pulser, serial.Serial):
+    def __init__(self, output_queue: Queue, rate_hz: float, com: str = '/dev/cu.usbmodem101', baud: int = 9600):
+        Pulser.__init__(self, output_queue, rate_hz)
+        serial.Serial.__init__(self, com, baud, timeout=.1)
+        print('Waiting for device...')
+        sleep(3)
+        print('Connected to ', self.name)
+
+    def _read_time_of_flight(self) -> float:
+        self.write(b'0')
+        return 1e-6 * float(str(self.readline().decode().strip('\r\n')))
+
+
+class MockPulser(Pulser):
+    def __init__(self, output_queue: Queue, rate_hz: float):
+        super().__init__(output_queue, rate_hz)
+        self._phase_delta = 2 * pi / rate_hz
+        self._phase = -self._phase_delta
+
+    def _read_time_of_flight(self) -> float:
+        self._phase += self._phase_delta
+        return (sin(self._phase) + 1) * 400e-6 + 150e-6
 
 
 class Plotter:
@@ -83,12 +104,12 @@ class Plotter:
 
             plot_data = copy(self._memory)
             for n in range(self._avg_len, len(plot_data)):
-                plot_data[n] = sum(self._memory[n-self._avg_len:n]) / self._avg_len
+                plot_data[n] = sum(self._memory[n - self._avg_len:n]) / self._avg_len
 
             self._ax.cla()
 
             alphas = [i / self._length_in_values for i in range(self._length_in_values)]
-            self._ax.scatter(range(len(plot_data)), plot_data, cmap=cm.winter_r, alpha=alphas)
+            self._ax.scatter(range(len(plot_data)), plot_data, alpha=alphas)
             self._ax.set_ylim([0, MAX_DISTANCE_CM])
             self._ax.yaxis.tick_right()
             self._ax.set_ylabel('Distance (cm)')
@@ -109,8 +130,7 @@ class Plotter:
 
 
 if __name__ == '__main__':
-
     queue = Queue()
-    pulser = Pulser(queue, rate_hz=20.0)
+    pulser = MockPulser(queue, rate_hz=20.0)
     plotter = Plotter(pulser, length_s=5.0, avg_len=5)
     plotter.start()
